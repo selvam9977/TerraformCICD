bf9565420693a03507ffc3fdc86af6d48d47a3f0
Selvam Govindasamy <selvam9977@gmail.com>
2023-02-17 13:45:14 -0700

    added new file

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -15,8 +15,8 @@ terraform {
 #}
 
 provider "aws" {
-  shared_config_files      = ["/home/ec2-user/.aws/config"]
-  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+  region                   = "us-west-2"
+  shared_credentials_files = "/home/ec2-user/.aws/credentials"
   profile                  = "default"  
 }
 
