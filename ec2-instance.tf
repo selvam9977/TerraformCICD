fcc1b65773f0f33a58132a495ccf12e6618aba39
Selvam Govindasamy <selvam9977@gmail.com>
2023-02-17 14:04:43 -0700

    added new file

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,21 +3,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.21" # Optional but recommended in production
+      #version = "~> 3.21" # Optional but recommended in production
     }
   }
 }
 
-# Provider Block
-#provider "aws" {
-  #profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  #region  = "us-west-2"
-#}
 
 provider "aws" {
   region                   = "us-west-2"
-  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
-  profile                  = "default"  
+  #shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+  #profile                  = "default"  
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"  
 }
 
 # Resource Block
